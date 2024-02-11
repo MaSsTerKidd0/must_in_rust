2230d4696e21e85df353e0c595b1d87e775bf51c
MaSsTerKidd0 <88079730+MaSsTerKidd0@users.noreply.github.com>
2024-02-12 00:53:15 +0200

    adding save rsa keys as pem format

--- src/must/ciphers_lib/rsa_crypto.rs
+++ src/must/ciphers_lib/rsa_crypto.rs
@@ -1,6 +1,10 @@
 use rsa::{RsaPrivateKey, RsaPublicKey, Oaep, sha2::Sha256};
 use rand::rngs::OsRng;
 use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use rsa::pkcs1::EncodeRsaPublicKey;
+use rsa::pkcs8::EncodePrivateKey;
 
 
 pub struct RsaCryptoKeys {
@@ -18,6 +22,21 @@ impl RsaCryptoKeys {
 
     pub fn get_public_key(&self) -> &RsaPublicKey {
         return &self.public_key;
+    }
+    pub fn save_keys(&self) -> Result<(), Box<dyn Error>> {
+        // Save the private key in PEM format
+        let private_key_pem = self.private_key.to_pkcs8_pem(Default::default())?;
+        let mut private_file = File::create("private_key.pem")?;
+        private_file.write_all(private_key_pem.as_bytes())?;
+
+        // Save the public key in PEM format
+        // Note: This assumes the `rsa` crate and your key types support converting to PKCS#1 PEM format.
+        // If your specific version or crate does not support this directly, you may need to adapt this code.
+        let public_key_pem = self.public_key.to_pkcs1_pem(Default::default())?;
+        let mut public_file = File::create("public_key.pem")?;
+        public_file.write_all(public_key_pem.as_bytes())?;
+
+        Ok(())
     }
     pub fn encrypt(&self, data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
         let mut rng = OsRng;
